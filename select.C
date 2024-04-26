b98e3369cffde84b1cea3d0f172c26fe66f619a9
Shrey Ramesh <105765663+shreynramesh@users.noreply.github.com>
2024-04-25 19:28:29 -0500

    rest of starter code and select.C

--- select.C
+++ select.C
@@ -1,15 +1,16 @@
 #include "catalog.h"
 #include "query.h"
-
+#include "stdio.h"
+#include "stdlib.h"
 
 // forward declaration
-const Status ScanSelect(const string & result, 
-			const int projCnt, 
-			const AttrDesc projNames[],
-			const AttrDesc *attrDesc, 
-			const Operator op, 
-			const char *filter,
-			const int reclen);
+const Status ScanSelect(const string &result,
+                        const int projCnt,
+                        const AttrDesc projNames[],
+                        const AttrDesc *attrDesc,
+                        const Operator op,
+                        const char *filter,
+                        const int reclen);
 
 /*
  * Selects records from the specified relation.
@@ -19,30 +20,127 @@ const Status ScanSelect(const string & result,
  * 	an error code otherwise
  */
 
-const Status QU_Select(const string & result, 
-		       const int projCnt, 
-		       const attrInfo projNames[],
-		       const attrInfo *attr, 
-		       const Operator op, 
-		       const char *attrValue)
-{
-   // Qu_Select sets up things and then calls ScanSelect to do the actual work
+const Status QU_Select(const string &result,
+                       const int projCnt,
+                       const attrInfo projNames[],
+                       const attrInfo *attr,
+                       const Operator op,
+                       const char *attrValue) {
+    Status status;
+    AttrDesc attrDescArray[projCnt];
+    AttrDesc *attrDesc = NULL;
+    int reclen = 0;
+
+    // Qu_Select sets up things and then calls ScanSelect to do the actual work
     cout << "Doing QU_Select " << endl;
 
+    // go through the projection list and look up each in the
+    // attr cat to get an AttrDesc structure (for offset, length, etc)
+
+    for (int i = 0; i < projCnt; i++) {
+        Status status = attrCat->getInfo(projNames[i].relName,
+                                         projNames[i].attrName,
+                                         attrDescArray[i]);
+        if (status != OK) {
+            return status;
+        }
+        reclen += attrDescArray[i].attrLen;
+    }
+
+    // get AttrDesc structure for the select attribute
+    if (attr != NULL) {
+        attrDesc = new AttrDesc;
+        status = attrCat->getInfo(attr->relName, attr->attrName, *attrDesc);
+        if (status != OK) {
+            return status;
+        }
+    }
+
+    return ScanSelect(result, projCnt, attrDescArray, attrDesc, op, attrValue, reclen);
 }
 
-
-const Status ScanSelect(const string & result, 
-#include "stdio.h"
-#include "stdlib.h"
-			const int projCnt, 
-			const AttrDesc projNames[],
-			const AttrDesc *attrDesc, 
-			const Operator op, 
-			const char *filter,
-			const int reclen)
-{
+const Status ScanSelect(const string &result,
+                        const int projCnt,
+                        const AttrDesc projNames[],
+                        const AttrDesc *attrDesc,
+                        const Operator op,
+                        const char *filter,
+                        const int reclen) {
     cout << "Doing HeapFileScan Selection using ScanSelect()" << endl;
 
+    Status status;
+    Record outRec;
+    RID tmpRid;
+    RID outRID;
+    Record tmpRec;
+    int tmpInt;
+    float tmpFloat;
 
+    // Opening resulting relation
+    InsertFileScan *resRel = new InsertFileScan(result, status);
+    if (status != OK) {
+        return status;
+    }
+
+    // Opening current table
+    HeapFileScan *scanRel = new HeapFileScan(projNames[0].relName, status);
+    if (status != OK) {
+        return status;
+    }
+
+    // Checking if unconditional scan is required
+    if (attrDesc == NULL) {
+        status = scanRel->startScan(0, 0, STRING, NULL, EQ);
+    } else {
+        switch (attrDesc->attrType) {
+            case STRING:
+                status = scanRel->startScan(attrDesc->attrOffset, attrDesc->attrLen, STRING, filter, op);
+            case INTEGER:
+                tmpInt = atoi(filter);
+                status = scanRel->startScan(attrDesc->attrOffset, attrDesc->attrLen, INTEGER, (char *)&tmpInt, op);
+            case FLOAT:
+                tmpFloat = atof(filter);
+                status = scanRel->startScan(attrDesc->attrOffset, attrDesc->attrLen, FLOAT, (char *)&tmpFloat, op);
+        }
+    }
+    if (status != OK) {
+        return status;
+    }
+
+    // Scanning relation
+    while ((status = scanRel->scanNext(tmpRid)) == OK) {
+        status = scanRel->getRecord(tmpRec);
+        if (status != OK) {
+            return status;
+        }
+
+        // Looping through specified projections to make output record
+        int outRecOffset = 0;
+        for (int i = 0; i < projCnt; i++) {
+            if (projNames[i].attrType == STRING) {
+                memcpy((char *)outRec.data + outRecOffset, (char *)tmpRec.data + projNames[i].attrOffset, projNames[i].attrLen);
+            } else if (projNames[i].attrType == INTEGER) {
+                memcpy((char *)outRec.data + outRecOffset, (int *)tmpRec.data + projNames[i].attrOffset, projNames[i].attrLen);
+            } else if (projNames[i].attrType == FLOAT) {
+                memcpy((char *)outRec.data + outRecOffset, (float *)tmpRec.data + projNames[i].attrOffset, projNames[i].attrLen);
+            }
+
+            outRecOffset += projNames[i].attrLen;
+        }
+
+        status = resRel->insertRecord(outRec, outRID);
+    }
+
+    // Checking if there was something wrong with the scan - should have reached EOF
+    if (status != FILEEOF) {
+        return status;
+    }
+
+    // Ending scan
+    status = scanRel->endScan();
+    if (status != OK) {
+        return status;
+    }
+
+    return OK;
 }
